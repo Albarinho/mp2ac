7144c614ec9eb58ddfaadfe046b7a3a922b4e812
albaro claros <ac484@njit.edu>
2018-11-27 19:19:27 -0500

    1.Wrote a feature test to check if the register return a 200 status code.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
 
     <div class=" mt-3 pl-2 pr-2">
 
-        <h1>My interest are:</h1>
+        <h1>My interests are:</h1>
         <div class="content">
             <ul class="navbar-nav mr-auto text-body=3px">
                 <li><a class="lead">Cars</a></li>
