8d058dcb0e32a71cb87029b79012166f1a7195e3
albaro claros <ac484@njit.edu>
2018-11-27 20:37:09 -0500

    6.Created a unit test to update the name of a user in the database to Steve Smith.

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -14,13 +14,27 @@ class UserTest extends TestCase
      *
      * @return void
      */
-
+/*
     public function testInsertUser()
     {
         $user = new User();
         $user->name ='John Cena';
-        $user->email ='youcantCme';
+        $user->email ='youcantCme@gmail.com';
         $user->password='123456';
         $this->assertTrue($user->save());
+    }*/
+//commented because everytime phpunit is run a new user will be created, not needed after the first test.
+
+
+    public function testUpdateUserName()
+    {
+        $user = User::all()->last();
+        $user-> where('name', 'John Cena') -> update(['name' => 'Steve Smith']);
+        $this->assertTrue($user->save());
     }
+
+
+
+
+
 }
